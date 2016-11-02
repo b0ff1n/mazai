6f5f9e5eb571a7f79d2dba1d8fd0046461ecbfd9
b0ff1n <eugene@amobee.com>
2016-11-02 12:47:15 +0200

    skel

---
+++ src/clj/mazai/core.clj
@@ -0,0 +1,14 @@
+(ns mazai.core
+  (:require [com.stuartsierra.component :as component]
+            [clojure.tools.reader.edn   :as edn]
+            [mazai.system       :refer [initialize-logger! new-system]])
+  (:gen-class))
+
+(def system nil)
+
+(defn -main [config-file]
+  (let [config (edn/read-string (slurp config-file))]
+    (initialize-logger! (:logger config))
+    (alter-var-root #'system (fn [_] (component/start (new-system config))))
+    (.addShutdownHook (Runtime/getRuntime)
+                      (Thread. #(component/stop system)))))
